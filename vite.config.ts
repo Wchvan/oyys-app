55aa5c2e432f5c9ad0c1be5463654ed10e1260e6
Van <1027154225@qq.com>
2023-07-01 23:40:20 +0800

    feature(custom): 完成前端基本架构

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    AutoImport({
+      imports: ['vue', 'uni-app'],
+      dts: './auto-imports.d.ts', // 安装好依赖后，重新运行编译即可自动在根目录下生成此声明文件
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~`': path.resolve(__dirname, '/'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/vars.scss";`,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4523/m1/2917564-0-default',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
